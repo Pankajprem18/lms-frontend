005ac954cb26e1ec12ab103b91e3995351cc656a
Pankajprem18 <pankajkrp16@gmail.com>
2024-07-23 23:33:24 +0530

    Added login signup button

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,10 +1,21 @@
-import { AiFillCloseCircle } from "react-icons/ai";
-import { FiMenu } from "react-icons/fi";
-import { Link } from 'react-router-dom';
-import Footer from '../Components/Footer';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { FiMenu } from 'react-icons/fi';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
 
+import Footer from '../Components/Footer';
+// import { logout } from '../Redux/Slices/AuthSlice';
 
 function HomeLayout({ children }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // for checking if user is logged in
+  const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+
+  // for displaying the options according to role
+  const role = useSelector((state) => state?.auth?.role);
+
   function changeWidth() {
     const drawerSide = document.getElementsByClassName("drawer-side");
     drawerSide[0].style.width = 'auto';
@@ -13,18 +24,25 @@ function HomeLayout({ children }) {
   function hideDrawer() {
     const element = document.getElementsByClassName("drawer-toggle");
     element[0].checked = false;
+
     const drawerSide = document.getElementsByClassName("drawer-side");
     drawerSide[0].style.width = '0';
   }
 
+  async function handleLogout(e) {
+    e.preventDefault();
+    // Uncomment the following lines when the logout function is implemented
+    // const res = await dispatch(logout());
+    // if (res?.payload?.success) navigate("/");
+    navigate("/");
+  }
 
   return (
-    <div className='min-h-[90h]'>
+    <div className='min-h-[90vh]'>
       <div className="drawer absolute left-0 z-50 w-fit">
         <input className="drawer-toggle" id="my-drawer" type="checkbox" />
         <div className="drawer-content">
           <label htmlFor="my-drawer" className="cursor-pointer relative">
-
             <FiMenu
               onClick={changeWidth}
               size={"32px"}
@@ -43,6 +61,11 @@ function HomeLayout({ children }) {
             <li>
               <Link to="/">Home</Link>
             </li>
+            {isLoggedIn && role === 'ADMIN' && (
+              <li>
+                <Link to="/admin/dashbord">Admin DashBoard</Link>
+              </li>
+            )}
             <li>
               <Link to="/courses">All Courses</Link>
             </li>
@@ -52,15 +75,35 @@ function HomeLayout({ children }) {
             <li>
               <Link to="/about">About Us</Link>
             </li>
+            <li className="mt-4 w-[90%]">
+              {!isLoggedIn && (
+                <div className="w-full flex items-center justify-center space-x-2">
+                  <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full bg-blue-500 text-white'>
+                    <Link to="/login">Login</Link>
+                  </button>
+                  <button className='btn-secondary px-4 py-1 font-semibold rounded-md w-full bg-green-500 text-white'>
+                    <Link to="/signup">Signup</Link>
+                  </button>
+                </div>
+              )}
+              {isLoggedIn && (
+                <div className="w-full flex items-center justify-center space-x-2">
+                  <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full bg-blue-500 text-white'>
+                    <Link to="/user/profile">Profile</Link>
+                  </button>
+                  <button className='btn-secondary px-4 py-1 font-semibold rounded-md w-full bg-red-500 text-white'>
+                    <Link onClick={handleLogout}>Logout</Link>
+                  </button>
+                </div>
+              )}
+            </li>
           </ul>
         </div>
       </div>
-
       {children}
-
       <Footer />
-
     </div>
   );
 }
+
 export default HomeLayout;
