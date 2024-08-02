9b217a9507a11154ffe58aa58652a856ea4d6c48
Pankajprem18 <pankajkrp16@gmail.com>
2024-08-02 19:30:17 +0530

    Added Complete signup flow

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import AboutUs from './Pages/AboutUs';
 import NotFound from './Pages/NotFound';
+import Signup from './Pages/Signup';
 function App() {
 
   return (
@@ -13,6 +14,8 @@ function App() {
    <Routes>
    <Route path = "/" element= {<HomePage/>} ></Route>
    <Route path="/about" element = {<AboutUs/>}></Route>
+
+   <Route path="/signup" element={<Signup/>}/>
 
    <Route path="*" element={<NotFound/>} ></Route>
 
